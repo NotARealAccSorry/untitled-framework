0f7bec2773f618142a1988a440887ac8af293829
NotARealAccSorry <pranavkannan2013@icloud.com>
2026-08-25 15:22:29 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,46 @@
 class Fighter {
-    character: Image
-    hurtbox: Sprite
-    speed: 100
-    damage: number
-    moveMeter: number
-
+    character: Image;
+    hurtbox: Image;
+    speed: number;
+    damage: number;
+    jumpHeight: number
+    maxJumps: number
+    moveset: Moveset
+constructor(character: Image, hurtbox: Image, speed: number, damage: number, jumpHeight: number, maxJumps: number, moveset: Moveset) {
+        this.character = character;
+        this.hurtbox = hurtbox;
+        this.speed = speed;
+        this.damage = damage;
+        this.jumpHeight = jumpHeight;
+        this.maxJumps = maxJumps;
+        this.moveset = moveset
+    }
 }
 class Moveset {
-    neutralAttack
+    neutralAttack:Attack;
+    sideAttack:Attack;
+    upAttack:Attack;
+    downAttack:Attack;
+    neutralSpecial:Attack;
+    sideSpecial:Attack;
+    upSpecial:Attack;
+    downSpecial:Attack;
+    constructor(neutralAttack: Attack,sideAttack: Attack,upAttack: Attack,downAttack: Attack,neutralSpecial: Attack,sideSpecial: Attack,upSpecial: Attack,downSpecial: Attack) {
+        this.neutralAttack = neutralAttack;
+        this.sideAttack = sideAttack;
+        this.upAttack = upAttack;
+        this.downAttack = downAttack;
+        this.neutralSpecial = neutralSpecial;
+        this.sideSpecial = sideSpecial;
+        this.upSpecial = upSpecial;
+        this.downSpecial = downSpecial;
+    }
 }
-class Attack {}
+
+
+class Attack {
+    damage : number
+    angle : number
+    knockback: number
+    
+}
